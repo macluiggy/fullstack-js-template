2c36b2e868889b09af326618290ebe9f0b8a3774
luiggy macias <ferrinluiggy@gmail.com>
2021-12-20 11:32:21 -0500

    trying to fix the error

--- backend/index.ts
+++ backend/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!sxd");
+  res.send("Hello World!");
 });
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+export default app;
--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -2,18 +2,38 @@ const path = require("path");
 
 module.exports = {
   entry: "./index.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
+      stream: false,
+      crypto: false,
+      "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
+      url: false,
+      util: false,
+      util: require.resolve("util"),
+      querystring: require.resolve("querystring-es3"),
+      buffer: require.resolve("buffer/"),
+    },
   },
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
