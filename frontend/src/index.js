4cd16e68cfdce2e150349988d90a9cc4da86dc10
luiggy macias <ferrinluiggy@gmail.com>
2021-12-20 14:50:54 -0500

    webpack config for frontend finished

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-const title = "Hello World";
-
-render(<div>{title}</div>, document.getElementById("app"));
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,24 +1,45 @@
+const webpack = require("webpack");
 const path = require("path");
+const CURRENT_WORKING_DIR = process.cwd() || __dirname;
+console.log(CURRENT_WORKING_DIR);
+
+const rulesForJavaScript = {
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  use: ["babel-loader"],
+};
+const rulesForTypescript = {
+  test: /\.tsx?$/,
+  use: "ts-loader",
+  exclude: /node_modules/,
+};
+const rulesForSass = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    "style-loader",
+    // Translates CSS into CommonJS
+    "css-loader",
+    // Compiles Sass to CSS
+    "sass-loader",
+  ],
+};
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.ts"),
+  entry: path.resolve(CURRENT_WORKING_DIR, "src/index.tsx"),
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: ["bable-loader"],
-      },
-    ],
+    rules: [rulesForJavaScript, rulesForTypescript, rulesForSass],
   },
   resolve: {
     extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
   },
   output: {
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(CURRENT_WORKING_DIR, "./dist"),
     filename: "bundle.js",
   },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
+    static: path.resolve(CURRENT_WORKING_DIR, "./dist"),
+    hot: true,
   },
 };
