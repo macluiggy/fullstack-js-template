13906611fdb74c5e92b9b35f9eae2d49863ef95a
luiggy macias <ferrinluiggy@gmail.com>
2021-12-22 09:23:43 -0500

    ignored pnpm-yaml.lock

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import indexRoute from "./routes/index.route";
+import cors from "cors";
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(cors()); //nunca te olvides de poner esto, si es que vas a usar las api de otro lado, osea de otro dominio o proxy
 
 //routes
 app.use("/", indexRoute);
